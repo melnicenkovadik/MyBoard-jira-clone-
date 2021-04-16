b9769c8b35aaa8590127496ee3b138f7e6524176
melnicenkovadik <melnicenkovadik@gmail.com>
2021-04-16 23:44:48 +0300

    build

--- .nuxt/dist/client/8177685.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{605:function(n,t,e){"use strict";e.r(t);var c={asyncData:function(){$nuxt.$fire.auth.signOut()}},l=e(56),component=Object(l.a)(c,(function(){var n=this,t=n.$createElement;return(n._self._c||t)("h1",[n._v("Sign out")])}),[],!1,null,null,null);t.default=component.exports}}]);
--- .nuxt/firebase/service.database.js
+++
@@ -1,9 +0,0 @@
-
-
-export default async function (session) {
-  await import('firebase/database')
-
-  const databaseService = session.database()
-
-  return databaseService
-}
