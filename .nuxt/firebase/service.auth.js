4e80f50793f3bc2c70250e5bbe3897340d47c669
melnicenkovadik <melnicenkovadik@gmail.com>
2021-04-16 23:34:58 +0300

    build

---
+++ .nuxt/firebase/service.auth.js
@@ -0,0 +1,48 @@
+
+
+export default async function (session, firebase, ctx, inject) {
+  await import('firebase/auth')
+
+  const authService = session.auth()
+
+    if (process.client) {
+    const persistence = firebase.auth.Auth.Persistence.LOCAL
+    try {
+      await authService.setPersistence(persistence)
+    } catch (err) {
+      if (err.code === 'auth/invalid-persistence-type') {
+        console.warn(`[@nuxtjs/firebase]: Invalid persistence type '${persistence}' provided`)
+      } else if (err.code === 'auth/unsupported-persistence-type') {
+        console.warn(`[@nuxtjs/firebase]: Persistence type '${persistence}' is not supported in this environment.`)
+      }
+    }
+  }
+
+  // Sets up a listener, mutations and action for every onAuthStateChanged by Firebase.
+  const fireAuthStore = {
+    unsubscribe() {
+      if (this.unsubscribeAuthStateListener) {
+        this.unsubscribeAuthStateListener()
+        delete this.unsubscribeAuthStateListener
+      }
+      if (this.unsubscribeIdTokenListener) {
+        this.unsubscribeIdTokenListener()
+        delete this.unsubscribeIdTokenListener
+      }
+    },
+    subscribe() {
+      return new Promise(resolve => {
+        this.unsubscribeAuthStateListener = authService.onAuthStateChanged(async authUser => {
+          const claims = authUser ? (await authUser.getIdTokenResult(true)).claims : null
+
+          await ctx.store.dispatch("onAuthStateChanged", { authUser, claims })
+
+          resolve()
+        })
+      })
+    }
+  }
+  inject('fireAuthStore', fireAuthStore)
+
+  return authService
+}
